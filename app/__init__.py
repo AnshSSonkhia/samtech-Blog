0a8e9ad9cd0ed2e72c0d5be177fdbb15c5590a8e
matata samuel <mattasamuel3@gmail.com>
2020-09-29 15:17:15 +0300

    added test

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
-
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
-
+bcrypt = Bcrypt()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'main.login'
