a8c059b5059d70f220993fe5691ea61b3a42a8ce
matata samuel <mattasamuel3@gmail.com>
2020-09-29 09:18:56 +0300

    added db url

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tyra:password@localhost/bloghub'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:sam2020@localhost/blog'
     
 class ProdConfig(Config):
     pass
