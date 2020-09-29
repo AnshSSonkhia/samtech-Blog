ed19f9abcfdfb873528145cdbd3edb729c5da8f5
matata samuel <mattasamuel3@gmail.com>
2020-09-29 15:19:13 +0300

    inserted secret key

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
+    SECRET_KEY ='1234'
     API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:sam2020@localhost/blog'
     
