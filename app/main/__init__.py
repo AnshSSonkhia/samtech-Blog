e94e2c2f704b3471cba492752c1950ad53c0a8fe
matata samuel <mattasamuel3@gmail.com>
2020-09-29 08:29:07 +0300

    updated main forms

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,4 +1,13 @@
 from flask import Blueprint
-
 main = Blueprint('main',__name__)
-from .views import *
+
+from . import views
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY']= '1234'
