9cd75599108d2df6d1599d0d8e4d7ece730ddd37
matata samuel <mattasamuel3@gmail.com>
2020-09-29 08:18:33 +0300

    updated config file

--- config.py
+++ config.py
@@ -1,24 +1,16 @@
 import os
 
 class Config:
-
-    QUOTE_URL = "http://quotes.stormconsultancy.co.uk/random.json"
-    DEBUG="True"
-    SECRET_KEY=''
-    SQLALCHEMY_TRACK_MODIFICATIONS=True
+    API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://tyra:password@localhost/bloghub'
     
-
- # email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-class TestConfig():
-    
-
 class ProdConfig(Config):
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    pass
 
-configurations = { 'development':Config, 'test':TestConfig, "production":ProdConfig }
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
--- manage.py
+++ manage.py
@@ -1,18 +1,23 @@
+from app import create_app, db
+from app.models import User,Post
 from flask_script import Manager,Server
-from flask_migrate import Migrate,MigrateCommand
-from app import create_app,db
-from app.models import Author,Blog, Blogcomment
+from  flask_migrate import Migrate, MigrateCommand
 
+
+# Creating app instance
 app = create_app('development')
 
-manager =  Manager(app)
+manager = Manager(app)
+manager.add_command('server',Server)
+
 migrate = Migrate(app,db)
-manager.add_command('runserver',Server(use_debugger=True))
 manager.add_command('db',MigrateCommand)
 
-@manager.shell
-def add_shell_context():
-    return {"db":db,"Author":Author}
 
-if __name__=="__main__":
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User ,Post=Post)
+
+
+if __name__ == '__main__':
     manager.run()
